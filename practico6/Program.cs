bc9f2a4401d43232eb61b327736f98b5b1041a2b
Luciano <lucianobonilla27@gmail.com>
2023-05-19 14:59:01 -0300

    calculadora v1 terminada

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,11 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// int a;
-// int b;
-// a=10;
-// b=a;
-// Console.WriteLine("valor de a:"+a);
-// Console.WriteLine("valor de b:"+b);
+﻿
 int menu,a,b;
 do
 {
@@ -19,16 +12,45 @@ bool control=int.TryParse(Console.ReadLine(),out menu);
 switch (menu)
 {
     case 1:
+    Console.WriteLine("ingrese el primer numero a sumar"); 
+    bool control1=int.TryParse(Console.ReadLine(),out a);  
+    Console.WriteLine("ingrese el segundo numero a sumar"); 
+    bool control2=int.TryParse(Console.ReadLine(),out b);  
+        Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
 
+
+       
     break;
      case 2:
-
+    Console.WriteLine("ingrese el primer numero a restar"); 
+    bool control3=int.TryParse(Console.ReadLine(),out a);  
+    Console.WriteLine("ingrese el segundo numero a restar"); 
+    bool control4=int.TryParse(Console.ReadLine(),out b);  
+    Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
+    
     break;
      case 3:
-
+    Console.WriteLine("ingrese el primer numero a multiplicar");
+    bool control5=int.TryParse(Console.ReadLine(),out a);  
+    Console.WriteLine("ingrese el segundo numero a multiplicar"); 
+    bool control6=int.TryParse(Console.ReadLine(),out b);  
+    Console.WriteLine("{0} x {1} = {2}",a,b,a*b);
+    
+    
     break;
      case 4:
+    Console.WriteLine("ingrese el primer numero a dividir");
+    bool control7=int.TryParse(Console.ReadLine(),out a);  
+    Console.WriteLine("ingrese el segundo numero a dividir"); 
+    bool control8=int.TryParse(Console.ReadLine(),out b);  
+    Console.WriteLine("{0} : {1} = {2}",a,b,a/b);
 
+    
+    break;
+
+    case 5:
+    Console.WriteLine("Adios!"); 
+    
     break;
     default:
     Console.WriteLine("Ingrese un numero valido"); 
