aa2740dbfd1ca9c5f3995578948a06ba9710d0b8
Luciano <lucianobonilla27@gmail.com>
2023-05-19 17:50:08 -0300

    ultimo commit de calculadora v1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,5 +1,6 @@
 ﻿
-int menu,a,b;
+int menu;
+float a,b;
 do
 {
 Console.WriteLine("ingrese la operacion que desea realizar"); 
@@ -13,9 +14,9 @@ switch (menu)
 {
     case 1:
     Console.WriteLine("ingrese el primer numero a sumar"); 
-    bool control1=int.TryParse(Console.ReadLine(),out a);  
+    bool control1=float.TryParse(Console.ReadLine(),out a);  
     Console.WriteLine("ingrese el segundo numero a sumar"); 
-    bool control2=int.TryParse(Console.ReadLine(),out b);  
+    bool control2=float.TryParse(Console.ReadLine(),out b);  
         Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
 
 
@@ -23,26 +24,26 @@ switch (menu)
     break;
      case 2:
     Console.WriteLine("ingrese el primer numero a restar"); 
-    bool control3=int.TryParse(Console.ReadLine(),out a);  
+    bool control3=float.TryParse(Console.ReadLine(),out a);  
     Console.WriteLine("ingrese el segundo numero a restar"); 
-    bool control4=int.TryParse(Console.ReadLine(),out b);  
+    bool control4=float.TryParse(Console.ReadLine(),out b);  
     Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
     
     break;
      case 3:
     Console.WriteLine("ingrese el primer numero a multiplicar");
-    bool control5=int.TryParse(Console.ReadLine(),out a);  
+    bool control5=float.TryParse(Console.ReadLine(),out a);  
     Console.WriteLine("ingrese el segundo numero a multiplicar"); 
-    bool control6=int.TryParse(Console.ReadLine(),out b);  
+    bool control6=float.TryParse(Console.ReadLine(),out b);  
     Console.WriteLine("{0} x {1} = {2}",a,b,a*b);
     
     
     break;
      case 4:
     Console.WriteLine("ingrese el primer numero a dividir");
-    bool control7=int.TryParse(Console.ReadLine(),out a);  
+    bool control7=float.TryParse(Console.ReadLine(),out a);  
     Console.WriteLine("ingrese el segundo numero a dividir"); 
-    bool control8=int.TryParse(Console.ReadLine(),out b);  
+    bool control8=float.TryParse(Console.ReadLine(),out b);  
     Console.WriteLine("{0} : {1} = {2}",a,b,a/b);
 
     
